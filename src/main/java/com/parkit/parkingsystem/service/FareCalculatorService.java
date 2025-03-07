7cf9813171bda3c6a0c66ea6fee28d3707cd0ad1
Dumas Valentin <122598380+Nano315@users.noreply.github.com>
2025-03-07 16:32:56 +0100

    feat: add free 30 minutes functionality with unit tests

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -11,19 +11,22 @@ public class FareCalculatorService {
             throw new IllegalArgumentException("Out time provided is incorrect:" + ticket.getOutTime());
         }
 
-        // Récupère les millisecondes depuis le 1er janvier 1970
         long inTimeMillis = ticket.getInTime().getTime();
         long outTimeMillis = ticket.getOutTime().getTime();
 
-        // Durée en heures : on divise par (1000 ms * 60 sec * 60 min)
         double durationInHours = (double) (outTimeMillis - inTimeMillis) / (1000 * 60 * 60);
 
-        // S’il y a eu une erreur et que c’est négatif, on peut lever une exception
         if (durationInHours < 0) {
             throw new IllegalArgumentException("Out time is earlier than in time.");
         }
 
-        // On applique le tarif en fonction du type de véhicule
+        // Implémentation de la gratuité pour moins de 30 minutes
+        if (durationInHours < 0.5) {
+            ticket.setPrice(0);
+            return; // on arrête la méthode, c'est gratuit
+        }
+
+        // Sinon, on applique le tarif normal
         switch (ticket.getParkingSpot().getParkingType()) {
             case CAR: {
                 ticket.setPrice(durationInHours * Fare.CAR_RATE_PER_HOUR);
