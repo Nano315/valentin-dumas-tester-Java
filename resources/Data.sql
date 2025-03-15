ae07ae6fb7d804be526324b2d6267508733fcd64
Dumas Valentin <122598380+Nano315@users.noreply.github.com>
2025-03-15 18:40:04 +0100

    tests d'intégration fonctionnel

--- resources/Data.sql
+++ resources/Data.sql
@@ -26,7 +26,7 @@ insert into parking(PARKING_NUMBER,AVAILABLE,TYPE) values(5,true,'BIKE');
 commit;
 
 /* Setting up TEST DB */
-create database test;
+--create database test;
 use test;
 
 create table parking(
